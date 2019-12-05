f3842e587df081e0244ab8e4545ea2c29ef2a74e
Eva Winkler <winkler.eva00@gmail.com>
2019-12-05 20:23:19 +0100

    uvoz podatkov za prosta in zasedena delovna mesta

--- uvoz/delovnamesta.R
+++
@@ -1,8 +0,0 @@
-library(readr)
-library(tidyr)
-library(dplyr)
-
-PROSTAM <- read_csv2("podatki/prostamesta.csv", col_names=c("prosta", "dejavnosti", 2008:2018 ), skip = 3, na="N", locale=locale(encoding = "Windows-1250"))
-
-
-
