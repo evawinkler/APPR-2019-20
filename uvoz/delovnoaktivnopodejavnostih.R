28e25a715ff66031cc15dd34fdef182298471b3d
Eva Winkler <eva.winkler@student.fmf.uni-lj.si>
2019-12-18 10:14:36 +0100

    spremenila obliko tabelic  za prosta in zasedena delovna mesta

--- uvoz/delovnoaktivnopodejavnostih.R
+++ uvoz/delovnoaktivnopodejavnostih.R
@@ -2,5 +2,6 @@ library(readr)
 library(tidyr)
 library(dplyr)
 
-DELOVNOAKTIVNO <- read_csv2("podatki/delovnoaktivnopodejavnostih.csv", col_names=c( "imena", 2010:2018 ), skip = 3, na="-", locale=locale(encoding = "Windows-1250"))
+DELOVNOAKTIVNO <- read_csv2("podatki/delovnoaktivnopodejavnostih.csv", col_names=c( "imena", 2010:2018 ), 
+                            skip = 3, na="-", locale=locale(encoding = "Windows-1250"))
 
--- uvoz/prostamesta.R
+++ uvoz/prostamesta.R
@@ -6,3 +6,5 @@ PROSTA <- read_csv2("podatki/prostamesta.csv", col_names=c("prosta", "dejavnosti
                     skip = 3, na="N", locale=locale(encoding = "Windows-1250"))%>%
   select(-"prosta")
 
+prosta.mesta <- gather(PROSTA, -dejavnosti, key=leto, value = prosta, na.rm = TRUE)
+prosta.mesta$leto <- parse_integer(prosta.mesta$leto)
