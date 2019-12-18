4fe2b623ab544497b17349fe81887ac2860a817c
Eva Winkler <eva.winkler@student.fmf.uni-lj.si>
2019-12-18 11:01:13 +0100

    spremenila oblike in imena

--- uvoz/prebivalstvogledenaaktivnost.R
+++ uvoz/prebivalstvogledenaaktivnost.R
@@ -3,21 +3,36 @@ library(tidyr)
 library(dplyr)
 
 
-moskivzhodna <- read_csv2("podatki/aktivnostmoskivzhodna.csv", col_names=c( "vzhodna", "razlogi ", "MOSKI", 2008:2018 ),
+MOSKIVZHODNA <- read_csv2("podatki/aktivnostmoskivzhodna.csv", col_names=c( "vzhodna", "razlogi", "MOSKI", 2008:2018 ),
                           skip = 3, na="N", locale=locale(encoding = "Windows-1250"))%>%
   select(-"vzhodna", -"MOSKI")
 
+moskivzhodna <- gather(MOSKIVZHODNA, -razlogi, key=leto, value = stevilo , na.rm = TRUE)
 
-moskizahodna <- read_csv2("podatki/aktivnostmoskizahodna.csv", col_names=c( "zahodna", "razlogi ", "MOSKI", 2008:2018 ), 
+moskivzhodna$leto <- parse_integer(moskivzhodna$leto)
+
+
+
+MOSKIZAHODNA <- read_csv2("podatki/aktivnostmoskizahodna.csv", col_names=c( "zahodna", "razlogi", "MOSKI", 2008:2018 ), 
                           skip = 3, na="N", locale=locale(encoding = "Windows-1250"))%>%
   select(-"zahodna", -"MOSKI")
 
+moskizahodna <- gather(MOSKIZAHODNA, -razlogi, key=leto, value = stevilo , na.rm = TRUE)
 
-zenskevzhodna <- read_csv2("podatki/aktivnostzenskevzhodna.csv", col_names=c( "vzhodna", "razlogi ", "zenske", 2008:2018 ),
+moskizahodna$leto <- parse_integer(moskizahodna$leto) 
+
+
+
+
+ZENSKEVZHODNA <- read_csv2("podatki/aktivnostzenskevzhodna.csv", col_names=c( "vzhodna", "razlogi", "zenske", 2008:2018 ),
                           skip = 3, na="N", locale=locale(encoding = "Windows-1250"))%>%
   select(-"vzhodna", -"zenske")
 
-zenskezahodna <- read_csv2("podatki/aktivnostzenskezahodna.csv", col_names=c( "zahodna", "razlogi ", "zenske", 2008:2018 ), 
+
+
+
+
+ZENSKEZAHODNA <- read_csv2("podatki/aktivnostzenskezahodna.csv", col_names=c( "zahodna", "razlogi", "zenske", 2008:2018 ), 
                           skip = 3, na="N", locale=locale(encoding = "Windows-1250"))%>%
   select(-"zahodna", -"zenske")
 
