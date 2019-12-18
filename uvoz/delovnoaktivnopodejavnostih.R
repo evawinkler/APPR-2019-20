fae47c1d87144f9e449ee5c2b9d2ac1ce4b3ce06
Eva Winkler <eva.winkler@student.fmf.uni-lj.si>
2019-12-18 11:01:24 +0100

    SPREMENILA TABELO

--- uvoz/delovnoaktivnopodejavnostih.R
+++ uvoz/delovnoaktivnopodejavnostih.R
@@ -5,3 +5,7 @@ library(dplyr)
 DELOVNOAKTIVNO <- read_csv2("podatki/delovnoaktivnopodejavnostih.csv", col_names=c( "imena", 2010:2018 ), 
                             skip = 3, na="-", locale=locale(encoding = "Windows-1250"))
 
+
+delovnoaktivno <- gather(DELOVNOAKTIVNO, -imena, key=leto, value = stevilo , na.rm = TRUE)
+delovnoaktivno$leto <- parse_integer(delovnoaktivno$leto)
+
