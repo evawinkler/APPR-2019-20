c3f4143d97f9602533f08a4b823758c5c2078e48
Eva Winkler <eva.winkler@student.fmf.uni-lj.si>
2019-12-18 11:09:07 +0100

    spremenila obliko

--- uvoz/placepodejavnostih.R
+++ uvoz/placepodejavnostih.R
@@ -7,3 +7,5 @@ PLACEPODEJAVNOSTIH <- read_csv2("podatki/placepodejavnostih.csv", col_names=c("n
                     skip = 3, na="-", locale=locale(encoding = "Windows-1250"))%>%
   select(-"neto")
 
+placepodejavnostih <- gather(PLACEPODEJAVNOSTIH, -dejavnosti, key=leto, value = stevilo , na.rm = TRUE)
+placepodejavnostih$leto <- parse_integer(placepodejavnostih$leto)
