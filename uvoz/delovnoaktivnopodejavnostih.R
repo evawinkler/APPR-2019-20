3c6ab9f3ce6b8168974f8ecfbbcc799c3b9d9429
Eva Winkler <winkler.eva00@gmail.com>
2019-12-23 20:56:14 +0100

    spremenila ime stolpca

--- uvoz/delovnoaktivnopodejavnostih.R
+++ uvoz/delovnoaktivnopodejavnostih.R
@@ -2,10 +2,10 @@ library(readr)
 library(tidyr)
 library(dplyr)
 
-DELOVNOAKTIVNO <- read_csv2("podatki/delovnoaktivnopodejavnostih.csv", col_names=c( "imena", 2010:2018 ), 
+DELOVNOAKTIVNO <- read_csv2("podatki/delovnoaktivnopodejavnostih.csv", col_names=c( "dejavnosti", 2010:2018 ), 
                             skip = 3, na="-", locale=locale(encoding = "Windows-1250"))
 
 
-delovnoaktivno <- gather(DELOVNOAKTIVNO, -imena, key=leto, value = stevilo , na.rm = TRUE)
+delovnoaktivno <- gather(DELOVNOAKTIVNO, -dejavnosti, key=leto, value = stevilo , na.rm = TRUE)
 delovnoaktivno$leto <- parse_integer(delovnoaktivno$leto)
 
