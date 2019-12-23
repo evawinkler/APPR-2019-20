ce78174ac4126c96271320c8332a0b037933c8c3
Eva Winkler <winkler.eva00@gmail.com>
2019-12-23 21:58:01 +0100

    preimenovala stolpec iz stevilo v del regije ker sem tako lahko zdruzila dve tabeli v eno

--- uvoz/prebivalstvogledenaaktivnost.R
+++ uvoz/prebivalstvogledenaaktivnost.R
@@ -7,16 +7,16 @@ MOSKIVZHODNA <- read_csv2("podatki/aktivnostmoskivzhodna.csv", col_names=c( "vzh
                           skip = 3, na="N", locale=locale(encoding = "Windows-1250"))%>%
   select(-"vzhodna", -"MOSKI")
 
-moskivzhodna <- gather(MOSKIVZHODNA, -aktivnost, key=leto, value = stevilo , na.rm = TRUE)
+moskivzhodna <- gather(MOSKIVZHODNA, -aktivnost, key=leto, value = vzhodna , na.rm = TRUE)
 
 moskivzhodna$leto <- parse_integer(moskivzhodna$leto)
 
 
 MOSKIZAHODNA <- read_csv2("podatki/aktivnostmoskizahodna.csv", col_names=c( "zahodna", "aktivnost", "MOSKI", 2008:2018 ), 
                           skip = 3, na="N", locale=locale(encoding = "Windows-1250"))%>%
-  select(-"zahodna", -"MOSKI")
+  select(-"zahodna",-"MOSKI" )
 
-moskizahodna <- gather(MOSKIZAHODNA, -aktivnost, key=leto, value = stevilo , na.rm = TRUE)
+moskizahodna <- gather(MOSKIZAHODNA, -aktivnost, key=leto, value = zahodna , na.rm = TRUE)
 
 moskizahodna$leto <- parse_integer(moskizahodna$leto) 
 
@@ -25,10 +25,10 @@ moskizahodna$leto <- parse_integer(moskizahodna$leto)
 
 ZENSKEVZHODNA <- read_csv2("podatki/aktivnostzenskevzhodna.csv", col_names=c( "vzhodna", "aktivnost", "zenske", 2008:2018 ),
                           skip = 3, na="N", locale=locale(encoding = "Windows-1250"))%>%
-  select(-"vzhodna", -"zenske")
+  select(-"vzhodna",-"zenske")
 
 
-zenskevzhodna <- gather(ZENSKEVZHODNA, -aktivnost, key=leto, value = stevilo , na.rm = TRUE)
+zenskevzhodna <- gather(ZENSKEVZHODNA, -aktivnost, key=leto, value = vzhodna , na.rm = TRUE)
 
 zenskevzhodna$leto <- parse_integer(zenskevzhodna$leto) 
 
@@ -36,9 +36,13 @@ zenskevzhodna$leto <- parse_integer(zenskevzhodna$leto)
 
 ZENSKEZAHODNA <- read_csv2("podatki/aktivnostzenskezahodna.csv", col_names=c( "zahodna", "aktivnost", "zenske", 2008:2018 ), 
                           skip = 3, na="N", locale=locale(encoding = "Windows-1250"))%>%
-  select(-"zahodna", -"zenske")
+  select(-"zahodna",-"zenske")
 
 
-zenskezahodna <- gather(ZENSKEZAHODNA, -aktivnost, key=leto, value = stevilo , na.rm = TRUE)
+zenskezahodna <- gather(ZENSKEZAHODNA, -aktivnost, key=leto, value = zahodna , na.rm = TRUE)
 
 zenskezahodna$leto <- parse_integer(zenskezahodna$leto) 
+
+
+moski <- full_join(moskivzhodna,moskizahodna)
+zenske <- full_join(zenskevzhodna,zenskezahodna)
