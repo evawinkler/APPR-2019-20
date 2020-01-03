efe10e62403117f8543795bc2b506cc7e89355e4
Eva Winkler <winkler.eva00@gmail.com>
2020-01-03 23:39:34 +0100

    spremenila datoteko

--- uvoz/placepodejavnostih.R
+++ uvoz/placepodejavnostih.R
@@ -1,12 +1,12 @@
 library(readr)
 library(tidyr)
 library(dplyr) 
+library("readxl")
+library("openxlsx")
 
+PLACEPODEJAVNOSTIH <- read_xlsx("podatki/placepodejavnostih1.xlsx",col_names=c("dejavnosti", 2008:2018),
+                                skip = 4, na="-")
 
-PLACEPODEJAVNOSTIH <- read_csv2("podatki/placepodejavnostih.csv", col_names=c("neto", "dejavnosti", 2008:2018 ),
-                    skip = 3, na="-", locale=locale(encoding = "Windows-1250"))%>%
-  select(-"neto")
 
 placepodejavnostih <- gather(PLACEPODEJAVNOSTIH, -dejavnosti, key=leto, value = stevilo , na.rm = TRUE)
 placepodejavnostih$leto <- parse_integer(placepodejavnostih$leto)
-
