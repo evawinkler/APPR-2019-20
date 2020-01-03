b164a0b84f1e330bfd902ab689102f248fa57f56
Eva Winkler <winkler.eva00@gmail.com>
2020-01-04 00:24:09 +0100

    spremembna oblike ko sem spremenila v xslx

--- uvoz/placeporegijah.R
+++ uvoz/placeporegijah.R
@@ -6,7 +6,7 @@ library("openxlsx")
 library(tmap)
 source("https://raw.githubusercontent.com/jaanos/APPR-2019-20/master/lib/uvozi.zemljevid.r")
 
-PLACEPOREGIJAH <- read_xlsx("podatki/placeporegijah1.xlsx", col_names=c( "regije", 2008:2018 ),
+PLACEPOREGIJAH <- read_xlsx("podatki/placeporegijah2.xlsx", col_names=c( "regije", 2008:2018 ),
                                 skip = 4, na="-")
 
 placeporegijah <- gather(PLACEPOREGIJAH, -regije, key=leto, value = stevilo , na.rm = TRUE)
@@ -16,11 +16,11 @@ povprecje.regije <- placeporegijah %>% group_by(regije) %>% summarise(povprecje=
 
 
 
-zemljevid <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_SVN_shp.zip", "gadm36_SVN_1", encoding = "Windows-1250")
+zemljevid <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_SVN_shp.zip", "gadm36_SVN_1", encoding = "UTF-8")
 
 
 
 names(zemljevid)
 
-tm_shape(merge(zemljevid, povprecje.regije, by.x="NAME_1", by.y="povprecje" ))+ tm_polygons("povprecje") 
+tm_shape(merge(zemljevid, povprecje.regije, by.x="NAME_1", by.y="regije" ))+ tm_polygons("povprecje") 
 
