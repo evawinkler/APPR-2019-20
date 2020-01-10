f2c2ca6ca7f0a7ed3a8136ad3d4ce7763f62090a
Eva Winkler <winkler.eva00@gmail.com>
2020-01-10 23:24:11 +0100

    dodala tabeli za plače leto 08 in 18

--- uvoz/placepodejavnostih.R
+++ uvoz/placepodejavnostih.R
@@ -1,5 +1,4 @@
 
-
 PLACEPODEJAVNOSTIH <- read_xlsx("podatki/placepodejavnostih1.xlsx",col_names=c("dejavnosti", 2008:2018),
                                 skip = 4, na="-")
 
@@ -7,7 +6,24 @@ PLACEPODEJAVNOSTIH <- read_xlsx("podatki/placepodejavnostih1.xlsx",col_names=c("
 placepodejavnostih <- gather(PLACEPODEJAVNOSTIH, -dejavnosti, key=leto, value = stevilo , na.rm = TRUE)
 placepodejavnostih$leto <- parse_integer(placepodejavnostih$leto)
 
-povprecje.dejavnosti <- placepodejavnostih %>% group_by(dejavnosti) %>% summarise(povprecje = sum(stevilo)/11)
+povprecje.dejavnosti <- placepodejavnostih %>% group_by(leto) %>% summarise(povprecje = sum(stevilo)/18)
 
 minmax <- placepodejavnostih %>% group_by(dejavnosti) %>% summarise(razlika = max(stevilo)- min(stevilo))
+
+PLACEPODEJAVNOSTIH08 <- read_xlsx("podatki/placepodejavnostih1.xlsx",col_names=c("dejavnosti", 2008:2018),
+                                skip = 4, na="-") %>% select(-"2009", - "2010", -"2011", -"2012",
+                                                             -"2013",-"2014", - "2015",
+                                                             -"2016", -"2017", -"2018")
+
+placepodejavnostih08 <- gather(PLACEPODEJAVNOSTIH1, -dejavnosti, key=leto, value = stevilo , na.rm = TRUE)
+
+
+
+PLACEPODEJAVNOSTIH18 <- read_xlsx("podatki/placepodejavnostih1.xlsx",col_names=c("dejavnosti", 2008:2018),
+                                 skip = 4, na="-") %>% select(-"2008", -"2009", - "2010", -"2011", -"2012",
+                                                              -"2013",-"2014", - "2015",
+                                                              -"2016", -"2017")
+placepodejavnostih18 <- gather(PLACEPODEJAVNOSTIH18, -dejavnosti, key=leto, value = stevilo , na.rm = TRUE)
+
+
 
