d60aed9e6b2a078c31ef8a21749f2ebd936dfda8
Eva Winkler <winkler.eva00@gmail.com>
2020-01-11 17:52:18 +0100

    dala vse v eno datoteko

--- uvoz/dejavnostiaktivnost.R
+++
@@ -1,6 +0,0 @@
-DEJAVNOSTI <- read_csv2("podatki/dejavnostiaktivnost.csv", col_names=c("drzava","spol", "dejavnosti", 2008:2018 ),
-                       skip = 3, na="N", locale=locale(encoding = "Windows-1250")) %>% 
-  select(-"drzava", - "spol")
-
-dejavnosti <- gather(DEJAVNOSTI, -dejavnosti, key=leto, value = stevilo, na.rm = TRUE)
-dejavnosti$leto <- parse_integer(dejavnosti$leto)
--- uvoz/izobrazbaaktivnost.R
+++
@@ -1,6 +0,0 @@
-IZOBRAZBA <- read_csv2("podatki/izobrazbaaktivnost.csv", col_names=c("regija","spol", "izobrazba", 2008:2018 ),
-                    skip = 3, na="N", locale=locale(encoding = "Windows-1250")) %>% 
-  select(-"regija", - "spol")
-
-izobrazba <- gather(IZOBRAZBA, -izobrazba, key=leto, value = stevilo, na.rm = TRUE)
-izobrazba$leto <- parse_integer(izobrazba$leto)
