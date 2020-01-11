4ff6eb1cc5a8c7f89463ba7d6c178b62855da9f1
Eva Winkler <winkler.eva00@gmail.com>
2020-01-11 17:53:58 +0100

    dodala ker sm pozabila

--- uvoz/uvoz.R
+++ uvoz/uvoz.R
@@ -1,5 +1,17 @@
 # ANALIZA PROSTIH IN ZASEDENIH MEST 
 
+PROSTA <- read_csv2("podatki/prostamesta.csv", col_names=c("prosta", "dejavnosti", 2008:2018 ),
+                    skip = 3, na="N", locale=locale(encoding = "Windows-1250")) %>% select(-"prosta")
+
+prosta.mesta <- gather(PROSTA, -dejavnosti, key=leto, value = prosta, na.rm = TRUE)
+prosta.mesta$leto <- parse_integer(prosta.mesta$leto)
+
+ZASEDENA <- read_csv2("podatki/zasedenamesta.csv",
+                      col_names=c("zasedena", "dejavnosti", 2008:2018 ), 
+                      skip = 3, na="N", locale=locale(encoding = "Windows-1250")) %>%
+  select(-"zasedena")
+
+
 zasedena.mesta <- gather(ZASEDENA, -dejavnosti, key=leto, value = zasedena, na.rm = TRUE)
 zasedena.mesta$leto <- parse_integer(zasedena.mesta$leto)
 
@@ -10,6 +22,9 @@ mesta <- full_join(prosta.mesta,zasedena.mesta)
 vsota.zasedena <- zasedena.mesta %>% group_by(leto) %>% summarise(vsota=sum(zasedena))
 
 vsota.prosta <- prosta.mesta %>% group_by(leto) %>% summarise(vsota=sum(prosta))
+
+
+
 
 
 # ANALIZA DELOVNO AKTIVNEGA PREBIVALSTVA 
