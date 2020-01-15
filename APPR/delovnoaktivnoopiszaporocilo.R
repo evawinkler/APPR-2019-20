f65e9fe8796490e9ed032e491f82d791d59f027c
Eva Winkler <winkler.eva00@gmail.com>
2020-01-15 19:47:39 +0100

    zbrisala

---
+++ APPR/delovnoaktivnoopiszaporocilo.R
@@ -0,0 +1,9 @@
+#Delovno aktivno prebivalstvo sem analizirala tudi z vidika dejavnosti.V času recesije bilo odpuščanje najbolj opazno v sekundarnih dejavnostih, kot so pridelovalne dejavnosti oz. proizvodnja, gradbeništvo.V ta namen sem naredila graf, saj je najbolj opazna sprememba.
+#Prav tako je v pridelovanih dejavnosti največ aktivnega prebivalstva zaposlenega, zato je vpliv brezposelnosti še večji.Veliko teh dejavnosti je v zasebnem sektorju,
+#kjer je odpuščanje večje kot v javnem sektorju. Javni sektor v času krize ne odpušča.
+#Stanje se je po krizi izboljšalo in smo bli leta 2018 oz. smo še zmeraj sedaj na boljšem 
+#kot pred krizo. 
+
+```{r g4, echo=FALSE, message=FALSE, warning=FALSE,fig.align='center', fig.cap='Gibanje števila zaposlenih v pridelovalnih dejavnostih v 1000'}
+g4
+```
