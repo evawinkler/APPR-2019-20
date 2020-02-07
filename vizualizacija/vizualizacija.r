ff296afd4cfe1f79c48340165305742de1a0b066
Eva Winkler <winkler.eva00@gmail.com>
2020-02-07 12:07:22 +0100

    dodala šumnike v imena koordinat, spremenila iz geom_point v geom_line

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -18,16 +18,16 @@ z1 <- tm_shape(merge(zemljevid, povprecje.regije, by.x="NAME_1", by.y="regije" )
 
 g <- ggplot() + aes(x=leto, y=vsota, color=tip) + facet_grid(tip ~ ., scales="free_y") +
   geom_line(data=mesta.vsote %>% filter(tip == "zasedena")) +
-  geom_point(data=mesta.vsote %>% filter(tip == "prosta")) + 
+  geom_line(data=mesta.vsote %>% filter(tip == "prosta")) + 
   scale_x_continuous(breaks=seq(2008, 2018, 2)) +
   scale_y_continuous(labels=comma_format(big.mark="")) +
   guides(color=FALSE) + ggtitle("Število prostih in zasedenih delovnih mest ")
 
-g3 <- ggplot(izobrazba %>% filter(izobrazba == "Brez izobrazbe, nepopolna osnovnošolska"))+ aes(x = leto, y = stevilo)  + geom_line(col="red") + ggtitle("Število aktivnega prebivalstva brez izobrazbe v 1000")
+g3 <- ggplot(izobrazba %>% filter(izobrazba == "Brez izobrazbe, nepopolna osnovnošolska"))+ aes(x = leto, y = stevilo)  + geom_line(col="red") + ggtitle("Število aktivnega prebivalstva brez izobrazbe v 1000") + xlab("leto") + ylab("število")
 
-g4 <- ggplot(dejavnosti %>% filter(dejavnosti == "Predelovalne dejavnosti"))+ aes(x = leto, y = stevilo)  + geom_line(col="green") + ggtitle("Gibanje števila zaposlenih v pridelovalnih dejavnostih v 1000") 
+g4 <- ggplot(dejavnosti %>% filter(dejavnosti == "Predelovalne dejavnosti"))+ aes(x = leto, y = stevilo)  + geom_line(col="green") + ggtitle("Gibanje števila zaposlenih v pridelovalnih dejavnostih v 1000") + xlab("leto") + ylab("število")
 
-g5 <- ggplot(tabelahtml %>% filter(spremenljivke == "Unemployment rate(in Percent)")) + aes(x = leto, y = stevilo)  + geom_point() +  ggtitle("Rast brezposelnosti v % ")   + theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1)) 
+g5 <- ggplot(tabelahtml %>% filter(spremenljivke == "Unemployment rate(in Percent)")) + aes(x = leto, y = stevilo, group = 1)  + geom_line(col="black") +  ggtitle("Rast brezposelnosti v % ")   + theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1)) + xlab("leto") + ylab("število")
 
 
 
