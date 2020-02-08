e2af25461d4b35979222d496b2d359c19fb62f4f
Eva Winkler <winkler.eva00@gmail.com>
2020-02-08 10:59:22 +0100

    napredna analiza za pomursko regijo

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -23,14 +23,31 @@ g <- ggplot() + aes(x=leto, y=vsota, color=tip) + facet_grid(tip ~ ., scales="fr
   scale_y_continuous(labels=comma_format(big.mark="")) +
   guides(color=FALSE) + ggtitle("Število prostih in zasedenih delovnih mest ")
 
-g3 <- ggplot(izobrazba %>% filter(izobrazba == "Brez izobrazbe, nepopolna osnovnošolska"))+ aes(x = leto, y = stevilo)  + geom_line(col="red") + ggtitle("Število aktivnega prebivalstva brez izobrazbe v 1000") + xlab("leto") + ylab("število")
+g3 <- ggplot(izobrazba %>% filter(izobrazba == "Brez izobrazbe, nepopolna osnovnošolska"))+ aes(x = leto, y = stevilo)  + geom_line(col="red") + ggtitle("Število aktivnega prebivalstva brez izobrazbe v 1000") + xlab("Leto") + ylab("Število")
 
-g4 <- ggplot(dejavnosti %>% filter(dejavnosti == "Predelovalne dejavnosti"))+ aes(x = leto, y = stevilo)  + geom_line(col="green") + ggtitle("Gibanje števila zaposlenih v pridelovalnih dejavnostih v 1000") + xlab("leto") + ylab("število") 
+g4 <- ggplot(dejavnosti %>% filter(dejavnosti == "Predelovalne dejavnosti"))+ aes(x = leto, y = stevilo)  + geom_line(col="green") + ggtitle("Gibanje števila zaposlenih v pridelovalnih dejavnostih v 1000") + xlab("Leto") + ylab("Število") 
 
-g5 <- ggplot(tabelahtml %>% filter(spremenljivke == "Unemployment rate(in Percent)")) + aes(x = leto, y = stevilo, group = 1)  + geom_line(col="black") +  ggtitle("Rast brezposelnosti v % ")   + theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1)) + xlab("leto") + ylab("število")
+g5 <- ggplot(tabelahtml %>% filter(spremenljivke == "Unemployment rate(in Percent)")) + aes(x = leto, y = stevilo, group = 1)  + geom_line(col="black") +  ggtitle("Rast brezposelnosti v % ")   + theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1)) + xlab("Leto") + ylab("Število")
 
 
-g6pomurska <- ggplot(placeporegijah %>% filter(regije == "Pomurska")) + aes( x = leto, y = stevilo) + geom_line() + geom_smooth(method = "loess")
+#napoved za pomursko regijo 
 
-g6osrednjeslovenska <- ggplot(placeporegijah %>% filter(regije == "Osrednjeslovenska")) + aes( x = leto, y = stevilo) + geom_point() 
+g6pomurska <- ggplot(placeporegijah %>% filter(regije == "Pomurska")) + 
+  aes(x = leto, y = stevilo) + geom_point() + geom_smooth(method = "gam", se = FALSE)
+
+mgam <- gam(stevilo ~ leto , data = placeporegijah)
+leta <- data.frame(leto = seq(2019,2022))
+napoved <- leta %>% mutate(stevilo = predict(mgam, .))
+
+tabela.napoved <- bind_rows(napoved, (placeporegijah %>%  filter(regije == "Pomurska"))[c(2,3)])
+
+graf.napoved <- ggplot(tabela.napoved, aes(x = leto, y = stevilo)) + 
+  geom_point() + scale_x_continuous(name = "Leto", breaks = seq(2008,2022,1)) + ylab("Plača") +
+  ggtitle("Povprečna plača v Pomurju") +
+  geom_smooth(method = 'gam', formula = y ~ x, se = FALSE) + theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1))
+
+
+
+#napoved za osrednjeslovensko regijo 
+
 
