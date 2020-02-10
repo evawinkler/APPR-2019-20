97e3731253ffe7ed2531504f0825cb63134923af
Eva Winkler <winkler.eva00@gmail.com>
2020-02-10 19:04:54 +0100

    zaključek

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -17,36 +17,4 @@ g4 <- ggplot(dejavnosti %>% filter(dejavnosti == "Predelovalne dejavnosti"))+ ae
 g5 <- ggplot(tabelahtml %>% filter(spremenljivke == "Unemployment rate(in Percent)")) + aes(x = leto, y = stevilo, group = 1)  + geom_line(col="black") +  ggtitle("Rast brezposelnosti v % ")   + theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1)) + xlab("Leto") + ylab("Število")
 
 
-#napoved za pomursko regijo 
 
-g6pomurska <- ggplot(placeporegijah %>% filter(regije == "Pomurska")) + 
-  aes(x = leto, y = stevilo) + geom_point() + geom_smooth(method = "gam", se = FALSE)
-
-mgam1 <- gam(stevilo ~ leto , data = placeporegijah %>% filter(regije == "Pomurska"))
-leta1 <- data.frame(leto = seq(2019,2022))
-napoved <- leta1 %>% mutate(stevilo = predict(mgam1, .))
-
-tabela.napoved <- bind_rows(napoved, (placeporegijah %>%  filter(regije == "Pomurska"))[c(2,3)])
-
-graf.napoved <- ggplot(tabela.napoved, aes(x = leto, y = stevilo)) + 
-  geom_point() + scale_x_continuous(name = "Leto", breaks = seq(2008,2022,1)) + ylab("Plača") +
-  ggtitle("Povprečna mesečna neto plača v Pomurju") +
-  geom_smooth(method = 'gam', formula = y ~ x, se = FALSE) + theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1))
-
-
-
-#napoved za osrednjeslovensko regijo 
-
-g6osrednjeslovenska <- ggplot(placeporegijah %>% filter(regije == "Osrednjeslovenska")) + 
-  aes(x = leto, y = stevilo) + geom_point() + geom_smooth(method = "gam", se = FALSE)
-
-mgam2 <- gam(stevilo ~ leto , data = placeporegijah %>% filter(regije == "Osrednjeslovenska"))
-leta2 <- data.frame(leto = seq(2019,2022))
-napoved1 <- leta2 %>% mutate(stevilo = predict(mgam2, .))
-
-tabela.napoved1 <- bind_rows(napoved1, (placeporegijah %>%  filter(regije == "Osrednjeslovenska"))[c(2,3)])
-
-graf.napoved1 <- ggplot(tabela.napoved1, aes(x = leto, y = stevilo)) + 
-  geom_point() + scale_x_continuous(name = "Leto", breaks = seq(2008,2022,1)) + ylab("Plača") +
-  ggtitle("Povprečna mesečna neto plača v osrednjeslovenski regiji") +
-  geom_smooth(method = 'gam', formula = y ~ x, se = FALSE) + theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1))
