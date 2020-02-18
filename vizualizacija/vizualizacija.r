3dd13c12d58845def32fec90c8eedeb5a36512c2
Eva Winkler <winkler.eva00@gmail.com>
2020-02-18 16:31:09 +0100

    zadnji popravek grafa

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -10,11 +10,20 @@ g <- ggplot() + aes(x=leto, y=vsota, color=tip) + facet_grid(tip ~ ., scales="fr
   scale_y_continuous(labels=comma_format(big.mark="")) +
   guides(color=FALSE) + ggtitle("Število prostih in zasedenih delovnih mest ")
 
-g3 <- ggplot(izobrazba %>% filter(izobrazba == "Brez izobrazbe, nepopolna osnovnošolska"))+ aes(x = leto, y = stevilo)  + geom_line(col="red") + ggtitle("Število aktivnega prebivalstva brez izobrazbe v 1000") + xlab("Leto") + ylab("Število")
+g3 <- ggplot(izobrazba %>% filter(izobrazba == "Brez izobrazbe, nepopolna osnovnošolska"))+ 
+    aes(x = leto, y = stevilo, binwidth = 1)  + geom_line(col="red") +
+    ggtitle("Število aktivnega prebivalstva brez izobrazbe v 1000") +
+    xlab("Leto") + ylab("Število") + scale_x_continuous(breaks=c(2008:2018))
 
-g4 <- ggplot(dejavnosti %>% filter(dejavnosti == "Predelovalne dejavnosti"))+ aes(x = leto, y = stevilo)  + geom_line(col="green") + ggtitle("Gibanje števila zaposlenih v pridelovalnih dejavnostih v 1000") + xlab("Leto") + ylab("Število") 
+g4 <- ggplot(dejavnosti %>% filter(dejavnosti == "Predelovalne dejavnosti"))+
+  aes(x = leto, y = stevilo)  + geom_line(col="green") +
+  ggtitle("Gibanje števila zaposlenih v pridelovalnih dejavnostih v 1000") + xlab("Leto") + ylab("Število") + scale_x_continuous(breaks=c(2008:2018))
 
-g5 <- ggplot(tabelahtml %>% filter(spremenljivke == "Unemployment rate(in Percent)")) + aes(x = leto, y = stevilo, group = 1)  + geom_line(col="black") +  ggtitle("Rast brezposelnosti v % ")   + theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1)) + xlab("Leto") + ylab("Število")
+g5 <- ggplot(tabelahtml %>% filter(spremenljivke == "Unemployment rate(in Percent)")) + 
+  aes(x = leto, y = stevilo, group = 1)  + geom_line(col="black") + 
+  ggtitle("Rast brezposelnosti v % ")   +
+  theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1)) + 
+  xlab("Leto") + ylab("Število")
 
 
 
